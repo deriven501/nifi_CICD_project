4770e895c96b18e2d228089a330875344c72050d
nghiemt2 <nghiemt2@wwu.edu>
2025-08-13 22:25:52 -0700

    more work on this project

--- scripts/prepare_inventory.sh
+++ scripts/prepare_inventory.sh
@@ -2,5 +2,5 @@
 IP=${terraform output -raw instance_public_ip}
 cd ../
 cat <<EOF>> inventory.ini
-$IP ansible_user=ubuntu ansible_ssh_private_key_file=/home/ubuntu/nifi.pem 
+$IP ansible_user=ubuntu ansible_ssh_private_key_file=./nifi-key.pem
 EOF
