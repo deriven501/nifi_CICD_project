b90418b9113e8f9d3bd24db7299ef55dd469c85f
nghiemt2 <nghiemt2@wwu.edu>
2025-08-15 09:50:46 -0700

    main.tf modified and jenkinsfile modified

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,6 +55,11 @@ resource "aws_instance" "t2_medium_ec2" {
   }
 }
 
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "${var.cluster_name}-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume.json
+}
+
 resource "aws_iam_role_policy_attachment" "eks_cluster_attach" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -75,6 +80,15 @@ resource "aws_iam_role" "eks_node_role" {
   assume_role_policy = data.aws_iam_policy_document.node_assume.json
 }
 
+resource "aws_iam_role_policy_attachment" "eks_node_attach" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+  ])
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = each.key
+}
 
 resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
