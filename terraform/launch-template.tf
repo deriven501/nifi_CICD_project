770d2c259d042f10cf0dde3c42af5cb63eb48ee9
nghiemt2 <nghiemt2@wwu.edu>
2025-08-28 20:41:52 -0700

    more changes

--- terraform/launch-template.tf
+++ terraform/launch-template.tf
@@ -1,6 +1,10 @@
+data "aws_ssm_parameter" "eks_al2_ami" {
+  name = "/aws/service/eks/optimized-ami/${var.cluster_version}/amazon-linux-2/recommended/image_id"
+}
+
 resource "aws_launch_template" "eks_nodes" {
   name = "${var.cluster_name}-node"  
-  image_id      = var.ami_id
+  image_id      = data.aws_ssm_parameter.eks_al2_ami.value
   instance_type = var.instance_type
    
   user_data = base64encode(<<-EOT
