d5c134f744bf467f3d819dea7fd12521a807c261
nghiemt2 <nghiemt2@wwu.edu>
2025-09-08 10:12:11 -0700

    changes

--- terraform/launch-template.tf
+++ terraform/launch-template.tf
@@ -7,7 +7,9 @@ resource "aws_launch_template" "eks_nodes" {
   image_id      = data.aws_ssm_parameter.eks_al2_ami.value
   instance_type = var.instance_type
   key_name      = "NiFi"
-  security_group_names = [aws_security_group.allow_ssh.id]
+  network_interfaces {
+    security_groups = [aws_security_group.allow_ssh.id]
+  }
 
   user_data = base64encode(<<-EOT
     #!/bin/bash
