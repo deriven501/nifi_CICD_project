fc1bd88137e7671fa8d7af3e88352a5e846a6b7f
nghiemt2 <nghiemt2@wwu.edu>
2025-09-12 23:03:11 -0700

    more changes

--- scripts/prepare_inventory.sh
+++ scripts/prepare_inventory.sh
@@ -2,5 +2,5 @@
 IP=$(terraform -chdir=terraform output -raw eks_cluster_endpoint)
 cat <<EOF>> inventory.ini
 [nifi]
-$IP ansible_user=ubuntu ansible_ssh_private_key_file=../terraform/nifi-key.pem
+$IP ansible_user=ubuntu ansible_ssh_private_key_file=./NiFi.pem
 EOF
