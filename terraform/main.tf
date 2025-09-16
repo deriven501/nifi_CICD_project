3a550c5258de525f36d535eec4ba72e94df62f45
nghiemt2 <nghiemt2@wwu.edu>
2025-09-15 17:50:55 -0700

    fix subnet reference

--- terraform/main.tf
+++ terraform/main.tf
@@ -160,7 +160,7 @@ resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "${var.cluster_name}-nodes"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = data.aws_subnets.default.ids
+  subnet_ids      = [aws_subnet.public.id, aws_subnet.public_b.id]
   
   scaling_config {
     desired_size = var.node_group_desired
