3fcb32a4e5c7509289ca7426b487844d38cd211d
nghiemt2 <nghiemt2@wwu.edu>
2025-10-14 16:23:46 -0700

    untracked some files

--- scripts/prepare_inventory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-IP=$(terraform -chdir=terraform output -raw eks_cluster_endpoint)
-cat <<EOF>> inventory.ini
-[nifi]
-$IP ansible_user=ubuntu ansible_ssh_private_key_file=./NiFi.pem
-EOF
